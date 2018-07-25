d8fe3dd7bd512da2e883b3cb4e176dd092bac4cf
ghoostqq <qiu.gits@gmail.com>
2018-07-25 15:03:52 +0900

    Nested routes. Still has bug. User#show now shoes user's profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :profiles
-  resources :users
+  resources :users do
+    resources :profiles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
