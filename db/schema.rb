1d939ec1524ca0f767bc4d39f1d8aef288975700
濱田恭香 <s1611540@u.tsukuba.ac.jp>
2018-07-25 14:14:19 +0900

    password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725015215) do
+ActiveRecord::Schema.define(version: 20180725050659) do
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180725015215) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
