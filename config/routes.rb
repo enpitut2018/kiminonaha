7e931b216c7d8cde5cef2c0d869ca3c215d7acaf
ghoostqq <qiu.gits@gmail.com>
2018-07-27 11:28:27 +0900

    Fix adding about_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
   resources :tags
   get '/login', to: 'sessions#new'
