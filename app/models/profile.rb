76a404c07ae9a519a7c527b87672f20fd8ae834d
ghoostqq <qiu.gits@gmail.com>
2018-07-27 10:19:00 +0900

    Able to chage tags upon changing elements: both add and delete

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,38 @@ class Profile < ApplicationRecord
 
   has_and_belongs_to_many :tags
 
+  def get_elements_array
+    elements = Array.new
+    if ev = self.Event
+      elements.push("Event:"+ev)
+    end
+    if pl = self.Place
+      elements.push("Place:"+pl)
+    end
+    if tags_texts = self.tags_text_split
+      elements += tags_texts
+    end
+    return elements
+  end
+
+  def update_tags_based_on_elements
+    elements = get_elements_array
+
+    # TagになっているElementを削除
+    # Elementに含まれないTagを削除
+    self.tags.all.each do |tag|
+      if elements.include?(tag.name)
+        elements.delete(tag.name)
+      else
+        tag.destroy!
+      end
+    end
+    # 残ったElementをTagにする
+    elements.each do |ele|
+      change_element_to_tags ele
+    end
+  end
+
   def change_event_to_tags_table
     if ev = self.Event
       change_element_to_tags ev
@@ -18,7 +50,6 @@ class Profile < ApplicationRecord
   end
 
   def change_tags_text_to_tags_table
-    # TODO: 全角スペース
     if tags_texts = self.tags_text_split
       tags_texts.each do |tag_text|
         change_element_to_tags tag_text
@@ -43,6 +74,7 @@ class Profile < ApplicationRecord
   end
 
   def tags_text_split
+    # TODO: 全角スペース
     if self.tags_text
       self.tags_text.split
     else
